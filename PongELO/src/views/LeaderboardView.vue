6ce79eda9a1f1cf8483fc1f0a06d262ac96657d4
BoundlessFate <villac3@rpi.edu>
2024-03-14 00:25:03 -0400

    working on data

---
+++ PongELO/src/views/LeaderboardView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+  export default {
+    mounted() {
+      this.fetchData()
+    },
+    data() {
+      return {
+        allData:"",
+        brotherOrdering:{},
+      }
+    },
+    methods: {
+      async fetchData() {
+        try {
+          const obj = await fetch(
+            'https://raw.githubusercontent.com/BoundlessFate/PongELOData/main/data.json'
+            ).then(res => res.json());
+          console.log(obj.brother);
+          this.data = obj;
+          sortData();
+        } catch (ex) {
+          console.log("Error in fetch");
+        }
+      },
+      sortData() {
+        
+      }
+    }
+  }
+</script>
